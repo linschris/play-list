8b1734a044fcd9808950408843987ce825241e64
Christopher Linscott <lchris@Christophers-MacBook-Pro.local>
2021-01-04 02:43:56 -0800

    server fixing

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-const inDevelopment = true;
+const inDevelopment = false;
 export default inDevelopment;
 
 ReactDOM.render(
--- server.js
+++ server.js
@@ -36,7 +36,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 
-app.get('*', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
+// app.get('*', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
 
 
 app.listen(PORT, () => {
